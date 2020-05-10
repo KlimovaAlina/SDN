9051879d2540777190e8f6f7636278faf9f582a5
Sergey Kodolov <35339452+SergeyKodolov@users.noreply.github.com>
2020-05-10 14:29:05 +0500

    update localization

--- dynamic management/controller source code/monitor.py
+++ dynamic management/controller source code/monitor.py
@@ -1,27 +1,35 @@
 from jnpr.junos import Device
 
-#формируем rpc запрос к маршрутизатору
+#form rpc request to a router
 def rpc_get (dev):
-    return dev.rpc.get_interface_information(detail=True, interface_name='ds-0/0/0:1')
+    return dev.rpc.get_interface_information(
+        detail=True, 
+        interface_name='ds-0/0/0:1'
+    )
 
-#запрос данных и их обработка
+#data request and processing
 def _get_info (dev, old = 0):
-    _filter = ['//speed', '//output-bps', '//queue[queue-number=0]/queue-counters-red-packets']
+    _filter = [
+        '//speed',
+        '//output-bps',
+        '//queue[queue-number=0]/queue-counters-red-packets'
+    ]
     old = int(rpc_get(dev).xpath(_filter[2])[0].text.strip())
     prev = int(rpc_get(dev).xpath(_filter[1])[0].text.strip())
     
     response = rpc_get(dev)
     out = []
 
+    #save data to an array [interface speed, transfer rate, lost packets]
     for f in _filter:
-        out.append(response.xpath(f)[0].text.strip()) #сохраняем данные в массив [скорость интерфейса, скорость передачи, потерянные пакеты]
+        out.append(response.xpath(f)[0].text.strip()) 
       
-    #сохраняем кол-во потерянных пакетов после предыдущего запроса       
+    #save the number of lost packets after the previous request       
     div    = int(out[2])-old
     old    = int(out[2])
     out[2] = int(div)
     
-    #пропускаем повторяющиеся данные    
+    #skip duplicate data    
     if (prev != out[1]):
         prev = out[1]
         return out
